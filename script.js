25593c004bda1f4606cd5e9ddd1f40b486190920
Dea Putri Evanti <deaputrievanti20@gmail.com>
2025-09-02 13:25:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple interaction for menu button
+document.querySelector(".menu").addEventListener("click", () => {
+    alert("Menu clicked! (Bisa diganti dengan navigasi)");
+  });
+  
+  // Chat button action
+  document.querySelector(".chat-btn").addEventListener("click", () => {
+    alert("Let's Chat clicked! (Hubungkan ke WhatsApp atau chat box)");
+  });
+  
